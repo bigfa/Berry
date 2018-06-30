c24745cf110deb789e2aa0128a55aa0f4ec3064b
bigfa <jigoulee@gmail.com>
2018-06-30 21:42:25 +0800

    feat: add archive template

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-define('BERRY_VERSION', '1.0.2');
+define('BERRY_VERSION', '1.0.3');
 define('BERRY_DEBUG', false);
 
 require('inc/setup.php');
---
+++ tpl/template-archive.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Template Name: 文章归档模版
+*/
+?>
+<?php get_header(); ?>
+<main class="main-content">
+        <section class="container">
+   <h2 class="block-title">
+                <?php the_title();?>
+            </h2>
+        <div class="list-archive-wrapper">
+            <?php
+            $args = array(
+                'posts_per_page' => -1,
+                'post_type' => array('post'),
+                'ignore_sticky_posts' => 1,
+            );
+            $the_query = new WP_Query( $args );
+            $year=0;
+            $mon=0;
+            $all = array();
+            $output = '';
+            $i= 0;
+            while ( $the_query->have_posts() ) : $the_query->the_post();
+                $i++;
+                $year_tmp = get_the_time('Y');
+                $mon_tmp = get_the_time('n');
+                $y = $year;
+                $m = $mon;
+                if ($mon != $mon_tmp && $mon > 0) $output .= '</ul></div>';
+                if ($year != $year_tmp) {
+                    $year = $year_tmp;
+                    $all[$year] = array();
+                }
+                if ($mon != $mon_tmp) {
+                    $i = 0;
+                    $mon = $mon_tmp;
+                    $output .= "<div class='list list--archive'><h3 class='month-title'>" . $year . ' - ' . $mon . '</h3>'  . "<ul class='blockGroup is-ordered'>" ;
+                }
+                if($i < 5) :
+                    $output .= '<li class="archive-item"><a class="archive-item-title" href="'.get_permalink() .'">' . get_the_title() . '</a></li>';
+                endif;
+            endwhile;
+            wp_reset_postdata();
+            $output .= '</ul></div>';
+            echo $output;      ?>
+        </div>
+  </section>
+</main>
+<?php get_footer(); ?>
